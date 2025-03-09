22bd5419bbacd2f146a3414b91cb580e889dd77b
BrunoASNascimento <bruno-asn@outlook.com>
2025-03-09 11:04:19 -0300

    Add initial project structure and configuration files

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,29 @@
+﻿// Screen Sound
+
+string messageWelcome = "Welcome to the Screen Sound!";
+
+void ShowMessageWelcome()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(messageWelcome);
+}
+
+void ShowMenuOptions()
+{
+    Console.WriteLine("1 - Register Band");
+    Console.WriteLine("2 - List Bands");
+    Console.WriteLine("3 - Evaluate Band");
+    Console.WriteLine("4 - Calculate average of the Band");
+    Console.WriteLine("0 - Exit");
+}
+
+ShowMessageWelcome();
+ShowMenuOptions();
