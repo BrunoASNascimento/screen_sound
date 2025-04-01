8c016561b0ca406cfd1555bdfbbcc43cd30d252d
BrunoASNascimento <bruno-asn@outlook.com>
2025-03-31 21:25:49 -0300

    Add user input handling to ShowMenuOptions method
    Updated ShowMenuOptions in Program.cs to prompt for user input,
    read and parse the input, and handle menu selection using a
    switch statement. Displays messages for valid options and an
    error message for invalid options.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -23,6 +23,33 @@ void ShowMenuOptions()
     Console.WriteLine("3 - Evaluate Band");
     Console.WriteLine("4 - Calculate average of the Band");
     Console.WriteLine("0 - Exit");
+
+    Console.Write("\nDigite sua opção: ");
+    string optionSelected = Console.ReadLine()!;
+    int optionSelectedInt = int.Parse(optionSelected);
+
+    switch (optionSelectedInt)
+    {
+        case 1:
+            Console.WriteLine("Você selecionou a opção:" + optionSelected);
+            break;
+        case 2:
+            Console.WriteLine("Você selecionou a opção:" + optionSelected);
+            break;
+        case 3:
+            Console.WriteLine("Você selecionou a opção:" + optionSelected);
+            break;
+        case 4:
+            Console.WriteLine("Você selecionou a opção:" + optionSelected);
+            break;
+        case 0:
+            Console.WriteLine("Até a próxima!");
+            break;
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+
 }
 
 ShowMessageWelcome();
