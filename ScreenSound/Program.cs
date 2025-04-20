c8b0e69f1eb435ef3e7980ca26d50d67a7a97c60
BrunoASNascimento <bruno-asn@outlook.com>
2025-04-20 17:53:13 -0300

    Add EvaluateBand and CalculateAverage methods
    - Added `EvaluateBand` method to handle band evaluation, including
      user input, validation, and updating the band's notes.
    - Added `CalculateAverage` method to compute and display the
      average of a band's notes.
    - Updated `ShowMenuOptions` to integrate the new methods:
      - Removed redundant `break;` in `case 1`.
      - Removed `Console.WriteLine` in `case 4` and replaced it with
        a call to `CalculateAverage`.
    - Enhanced `ShowTitle` to support new methods.

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -42,7 +42,6 @@ void ShowMenuOptions()
     {
         case 1:
             RegisterBand();
-
             break;
         case 2:
             ShowAllBands();
@@ -51,7 +50,7 @@ void ShowMenuOptions()
             EvaluateBand();
             break;
         case 4:
-            Console.WriteLine("Você selecionou a opção:" + optionSelected);
+            CalculateAverage();
             break;
         case 0:
             Console.WriteLine("Até a próxima!");
@@ -113,6 +112,45 @@ void ShowTitle(string title)
 
 void EvaluateBand()
 {
+    Console.Clear();
+    ShowTitle("Evaluate Band");
+    Console.Write("Write the name of band: ");
+    string bandName = Console.ReadLine()!;
+    if (bands.ContainsKey(bandName))
+    {
+        Console.Write($"Write the note for {bandName}: ");
+        string note = Console.ReadLine()!;
+        int noteInt = int.Parse(note);
+        bands[bandName].Add(noteInt);
+        Console.WriteLine($"The note {note} was added to the band {bandName}");
+    }
+    else
+    {
+        Console.WriteLine($"The band {bandName} does not exist.");
+    }
+    Thread.Sleep(2000);
+    Console.Clear();
+    ShowMenuOptions();
+}
+
+void CalculateAverage()
+{
+    Console.Clear();
+    ShowTitle("Calculate average of the Band");
+    Console.Write("Write the name of band: ");
+    string bandName = Console.ReadLine()!;
+    if (bands.ContainsKey(bandName))
+    {
+        List<int> notes = bands[bandName];
+        double average = notes.Average();
+        Console.WriteLine($"The average of the band {bandName} is {average}");
+    }
+    else
+    {
+        Console.WriteLine($"The band {bandName} does not exist.");
+    }
+    Thread.Sleep(2000);
+    Console.Clear();
 }
 
 ShowMenuOptions();
